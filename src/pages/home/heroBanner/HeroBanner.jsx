ca60de47205ff6074c40f7d24869233a24df8239
Andrew <andrewmoren93@gmail.com>
2023-04-02 23:50:43 +0300

    create useFetch

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -3,10 +3,14 @@ import { useNavigate } from "react-router-dom";
 
 import "./style.scss";
 
+import useFetch from "../../../hooks/useFetch";
+
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+
+  const { data, loading } = useFetch("/movie/upcoming");
 
   const searchQueryHandler = (e) => {
     if (e.key === "Enter" && query.length > 0) {
